a401a89fa5254c711b9966fca406acf9a5a1ea7c
meows <b5c6@protonmail.com>
2020-03-04 06:58:22 -0600

    Revise to be ETH friendly
    Signed-off-by: meows <b5c6@protonmail.com>

--- .ci/filter_and_sign.sh
+++ .ci/filter_and_sign.sh
@@ -14,7 +14,7 @@ for network in "$@"; do
     mkdir -p "les.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
     devp2p nodeset filter all.json -les-server -eth-network "$network" >"les.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}/nodes.json"
 
-    
+
     echo "Sign: $network"
 
     for p in "${proto_groups[@]}"; do
@@ -26,14 +26,6 @@ for network in "$@"; do
         echo
         cat "${ETH_DNS_DISCV4_KEYPASS_PATH}" | devp2p dns sign "${p}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}" "${ETH_DNS_DISCV4_KEY_PATH}" && echo "OK"
 
-        git add "${p}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
     done
-
-    ETH_DNS_DISCV4_KEY_PUBLICINFO="$(cat $ETH_DNS_DISCV4_KEYPASS_PATH | ethkey inspect $ETH_DNS_DISCV4_KEY_PATH | grep -E '(Addr|Pub)')"
-    git -c user.name="meows" -c user.email='b5c6@protonmail.com' commit --author "crawler <>" -m "ci update ($network) $GITHUB_RUN_ID:$GITHUB_RUN_NUMBER
-        
-Crawltime: $ETH_DNS_DISCV4_CRAWLTIME
-
-$ETH_DNS_DISCV4_KEY_PUBLICINFO"
     
 done
